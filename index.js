51147b77a8694c17fcb6488ea492daac38faa7e9
Sina Bayati <bayati.sina.w@gmail.com>
2024-06-07 09:19:37 +0330

    Add "prettyPrint" function

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 import { Tree } from "./modules/tree.js";
 
-const T = new Tree([1,2,2,1,3]);
-console.log(T.root);
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+const T = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+// console.log(T.root);
+console.log(prettyPrint(T.root));
