c846a2c3c844f1e4df90f1bd5ac25c4e80597ba2
Sina Bayati <bayati.sina.w@gmail.com>
2024-06-18 11:52:32 +0330

    Implement "deleteItem" function

--- index.js
+++ index.js
@@ -16,4 +16,7 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 const T = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 // console.log(T.root);
 T.insert(69);
+T.insert(10000);
+T.insert(10001);
+T.root = T.deleteItem(67);
 prettyPrint(T.root);
--- modules/tree.js
+++ modules/tree.js
@@ -19,4 +19,42 @@ export class Tree{
 
     return root;
   }
+
+  deleteItem(key,root = this.root){
+    if(root === null){
+      return root;
+    }
+    // if the key to be removed is bigger than the root node 
+    if(key > root.data){
+      root.right = this.deleteItem(key,root.right);
+    }
+    // if the key to be removed is smaller than the root node 
+    else if(key < root.data){
+      root.left = this.deleteItem(key,root.left);
+    }
+    // if the key to be removed the root node 
+    else {
+      if(root.left === null){
+        return root.right;
+      } else if(root.right === null){
+        return root.left;
+      }
+
+      const data = this.minValue(root.right);
+      root.right = this.deleteItem(data,root.right);
+      root.data = data;
+    }
+    return root;
+  }
+
+  minValue(root){
+    let minValue = root.data;
+
+    while(root.left !== null){
+      minValue = root.left.data;
+      root = root.left;
+    }
+
+    return minValue;
+  }
 }
